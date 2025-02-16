4d3852949acdd1664e46158462b3786cde976cf2
revirevy <revirevy@gmail.Com>
2025-02-16 22:48:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def link_additional_models():
         print(x)
         if 'lora' in x:
             xlinkthis(f"/kaggle/input/dmd2/pytorch/default/1/{x}", f"/kaggle/working/ComfyUI/models/loras/{x}")
-        else
+        else:
             xlinkthis(f"/kaggle/input/dmd2/pytorch/default/1/{x}", f"/kaggle/working/ComfyUI/models/checkpoints/{x}")
 
     # /kaggle/input/juggernaut/pytorch/default/1
